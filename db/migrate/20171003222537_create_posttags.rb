0ccf90ccf68ea9917ce02a82764b7b2a8e89c91d
Timothy Cevallos <timcev@gmail.com>
2017-10-03 22:26:34 +0000

    automatically backed up by learn

---
+++ db/migrate/20171003222537_create_posttags.rb
@@ -0,0 +1,10 @@
+class CreatePosttags < ActiveRecord::Migration
+  def change
+    create_table :posttags do |t|
+      t.integer :user_id
+      t.integer :post_id
+
+      t.timestamps null: false
+    end
+  end
+end

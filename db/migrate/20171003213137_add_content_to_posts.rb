1e1e2ba3f2b6d5ce04e31545fb1074bbf3bd213f
Timothy Cevallos <timcev@gmail.com>
2017-10-03 22:30:43 +0000

    automatically backed up by learn

--- db/migrate/20171003213137_add_content_to_posts.rb
+++ db/migrate/20171003213137_add_content_to_posts.rb
@@ -1,7 +1,5 @@
 class AddContentToPosts < ActiveRecord::Migration
   def change
     add_column :posts, :content, :text
-    add_column :posts, :user_id, :integer
-    add_column :posts, :tag_id, :integer
   end
 end

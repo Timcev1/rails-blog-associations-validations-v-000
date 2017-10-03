3cb1c2e42839998a773fb29d4c97b3b334eade54
Timothy Cevallos <timcev@gmail.com>
2017-10-03 22:29:38 +0000

    automatically backed up by learn

--- db/migrate/20171003222938_create_post_tags.rb
+++ db/migrate/20171003222938_create_post_tags.rb
@@ -1,8 +1,6 @@
 class CreatePostTags < ActiveRecord::Migration
   def change
     create_table :post_tags do |t|
-      t.integer :tag_id
-      t.integer :post_id
 
       t.timestamps null: false
     end
